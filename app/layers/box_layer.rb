f585485143a23662e195d8a63f02a4b84eef93cc
Ryan Jones <ryan@system88.com>
2013-11-17 14:25:09 -0700

    new sprites like a boss

--- app/layers/box_layer.rb
+++ app/layers/box_layer.rb
@@ -7,53 +7,43 @@ class BoxLayer < Joybox::Core::Layer
   end
 
   def load_sprite_sheet
-    SpriteFrameCache.frames.add file_name: "sprites/sprites.plist"
-    @sprite_batch = SpriteBatch.new file_name: "sprites/sprites.png"
+    SpriteFrameCache.frames.add file_name: "sprites/stars.plist"
+    @sprite_batch = SpriteBatch.new file_name: "sprites/stars.png"
     self << @sprite_batch
 
+    @blue_box = Bucket.new({frame_name: 'blue_box.png', position: [300,300], home_position: [100,100]})
 
-    @wood_block = Bucket.new({frame_name: 'Wood Block.png', position: [300,300], home_position: [100,100]})
-    @gem_green = Gem.new({frame_name: 'Gem Green.png', position: [100,200], home_position: [100,200]})
-    @gem_blue = Gem.new({frame_name: 'Gem Blue.png', position: [100,300], home_position: [100,300]})
-    @gem_orange = Gem.new({frame_name: 'Gem Orange.png', position: [100,100], home_position: [100,100]})
 
-    @sprite_batch << @gem_green
-    @sprite_batch << @gem_blue
-    @sprite_batch << @gem_orange
-    @sprite_batch << @wood_block
+    @star_green = Gem.new({frame_name: 'green_star.png', position: [100,200], home_position: [100,200]})
+    @star_blue = Gem.new({frame_name: 'blue_star.png', position: [100,300], home_position: [100,300]})
+    @star_orange = Gem.new({frame_name: 'red_star.png', position: [100,100], home_position: [100,100]})
 
-    self.reorderChild(@gem_green, z: 10)
-    self.reorderChild(@gem_blue, z: 10)
-    self.reorderChild(@gem_orange, z: 10)
-    self.reorderChild(@wood_block, z: 1)
+    @sprite_batch << @star_green
+    @sprite_batch << @star_blue
+    @sprite_batch << @star_orange
+    @sprite_batch << @blue_box
 
-    @gems = Array.new
-    [@gem_green, @gem_blue, @gem_orange].each do |g|
-      @gems << g
+    @stars = Array.new
+    [@star_green, @star_blue, @star_orange].each do |g|
+      @stars << g
     end
   end
 
   def handle_touches
     on_touches_began do |touches, event|
       touch = touches.any_object
-      @gems.each do |g|
+      @stars.each do |g|
         if g.touched?(touch.location)
           g.movable = true
-
-          # extend sprite bathc to redraw the child
-          @sprite_batch.removeChild(g)
-          @sprite_batch << g
-
-          #store the z index on the child so we can always know the z index
-          self.reorderChild(g, z: 100)
+          sprite_to_front(g)
         end
       end
     end
 
     on_touches_moved do |touches, event|
       touch = touches.any_object
-      @gems.each do |g|
-        if g.movable == true
+      @stars.each do |g|
+        if g.movable
           g.position = touch.location
         end
       end
@@ -61,8 +51,8 @@ class BoxLayer < Joybox::Core::Layer
 
     on_touches_ended do |touches, event|
       touch = touches.any_object
-      @gems.each do |g|
-        if g.movable == true
+      @stars.each do |g|
+        if g.movable
           move_to_action = Move.to position: g.home_position
           g.run_action move_to_action
           g.movable = false
@@ -71,5 +61,8 @@ class BoxLayer < Joybox::Core::Layer
     end
   end
 
-
+  def sprite_to_front(sprite)
+    @sprite_batch.removeChild(sprite)
+    @sprite_batch << sprite
+  end
 end
